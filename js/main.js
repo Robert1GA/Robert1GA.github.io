72a441e664d5ec9a5fd160566fc9723e227fc7b5
Robert1GA <roberts.2015@yahoo.co.uk>
2017-02-14 15:06:05 -0800

    structure of navbar and start of name banner

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("JS good to go, sir!")
+$(document).ready(function() {
+  console.log("JS good to go, sir!")
+
+  $('#job-position').typeIt({
+    strings: 'Front-End Web Developer',
+    speed: 100,
+    startDelay: 400
+  })
+})
